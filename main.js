780eae6e3c01094df1da4cd967b15e59cc8341bf
Laiza <laiza.paini@escola.pr.gov.br>
2026-07-07 21:51:15 -0300

    alt 22

--- main.js
+++ main.js
@@ -1,9 +1,14 @@
 const numeroSenha = document.querySelector('.parametro-senha__texto');
 let tamanhoSenha = 12;
 numeroSenha.textContent = tamanhoSenha;
-
+const letrasMaiusculas = 'ABCDEFGHIJKLMNOPQRSTUVXYWZ';
+const letrasMinusculas = 'abcdefghijklmnopqrstuvxywz';
+const numeros = '0123456789';
+const simbolos = '!@%*?';
 const botoes = document.querySelectorAll('.parametro-senha__botao');
-
+const campoSenha = document.querySelector('#campo-senha');
+const checkbox = document.querySelectorAll('.checkbox');
+const forcaSenha = document.querySelector('.forca');
 botoes[0].onclick = diminuiTamanho
 botoes[1].onclick = aumentaTamanho;
 
@@ -15,7 +20,6 @@ function diminuiTamanho(){
       numeroSenha.textContent = tamanhoSenha;
       geraSenha();
 }
-
 function aumentaTamanho(){
     if(tamanhoSenha < 20){
         //tamanhoSenha = tamanhoSenha+1;
@@ -24,19 +28,12 @@ function aumentaTamanho(){
     numeroSenha.textContent = tamanhoSenha
     geraSenha();
 }
-const campoSenha = document.querySelector('#campo-senha');
-const checkbox = document.querySelectorAll('.checkbox')
 
 for (i=0; i < checkbox.length;i++){
     checkbox[i].onclick = geraSenha;
 }
 
 console.log(checkbox[1].checked);
-
-const letrasMaiusculas = 'ABCDEFGHIJKLMNOPQRSTUVXYWZ';
-const letrasMinusculas = 'abcdefghijklmnopqrstuvxywz';
-const numeros = '0123456789';
-const simbolos = '!@%*?';
 
 geraSenha();
 
@@ -54,7 +51,6 @@ function geraSenha(){
     if (checkbox[3].checked){
         alfabeto = alfabeto + simbolos;
     }
-    console.log(alfabeto);
     let senha = '';
  for (let i = 0; i < tamanhoSenha;i++) {
      let numeroAleatorio =  Math.random()*alfabeto.length;
@@ -62,6 +58,20 @@ function geraSenha(){
      senha = senha + alfabeto[numeroAleatorio];
  }
  campoSenha.value = senha;
+ classificaSenha(alfabeto.length);
 }
 
-campoSenha.value = senha;
+function classificaSenha(tamhanhoAlfabeto){
+     let entropia = tamanhoSenha * Math.log2(tamhanhoAlfabeto);
+    console.log(entropia);
+    forcaSenha.classList.remove('fraca','media','forte');
+        if (entropia > 57){
+    forcaSenha.classList.add('forte');
+} else if (entropia > 35 && entropia < 57 ) {
+    forcaSenha.classList.add('media');
+} else if (entropia <= 35){
+    forcaSenha.classList.add('fraca');
+}
+const valorEntropia = document.querySelector('.entropia');
+valorEntropia.textContent = 2**Math.floor(entropia)/(100e6*60*60*24);
+}   
